54ee068d2ecef874bf3002c339daf559b913fc47
eRodrigoL <159098656+eRodrigoL@users.noreply.github.com>
2024-08-17 00:21:55 +0000

    aula 16/08/2024

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,42 @@
 import { Text, View, StyleSheet} from "react-native";
 
+const produtos = [
+  { id: 1, nome: "Coca-Cola", preco: 5.5 },
+  { id: 2, nome: "Pepsi", preco: 5.5 },
+  { id: 3, nome: "Fanta", preco: 5.5 },
+  { id: 4, nome: "H2Oh", preco: 5.5 },
+];
+
 export default function Index() {
   return (
     <View
       style={estilo.container}
     >
-      <Text style={estilo.text}>Olá mundo!!</Text>
+      {produtos.map((p)=>(
+        <View key={p.id}>
+          <Text style={estilo.title}>{p.nome}</Text>
+          <Text style={estilo.text}>{p.preco}</Text>
+        </View>
+      ))}
     </View>
   );
 }
 
-
-const estilo = StyleSheet.create({
-  container: {
+const estilo= StyleSheet.create ({
+  container:{
     flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#FFF000",
-  },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:"#000000",
+},
   text: {
-    color: "#000000"
+    color:"#ffffff",
+    fontSize: 20,
+  },
+  title: {
+    color:"#FA5858",
+    fontSize: 50,
+    textAlign: "left",
+    fontWeight: "bold",
   }
-})
+});
